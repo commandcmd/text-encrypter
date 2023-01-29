c81ae0185fbfad256e3f7554b3b9af5775328b3f
Enrico <enrico.zan07@gmail.com>
2023-01-29 22:22:54 +0100

    Removed unnecessary charMap

--- charMap.java
+++
@@ -1,7 +0,0 @@
-public class charMap {
-    public static void main(String args[]){
-        for(int i = 0;i < 256;i++){
-            System.out.println((int)i + " : " + (char)i);
-        }
-    }
-}
